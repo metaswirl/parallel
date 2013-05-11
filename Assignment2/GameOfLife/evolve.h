82460cddb4bdefe3fe197cc9e17731d978dc5894
Ramkumar <cbe.ram@gmail.com>
2013-05-11 14:13:30 +0200

    Changed the directory structure.

---
+++ Assignment2/GameOfLife/evolve.h
@@ -0,0 +1,14 @@
+//
+//  evolve.h
+//  GameOfLife
+//
+//  Created by JIANNAN GUO on 10.05.13.
+//  Copyright (c) 2013 Jiannan Guo. All rights reserved.
+//
+
+#ifndef GameOfLife_evolve_h
+#define GameOfLife_evolve_h
+
+struct cell_list * evolve(struct cell_list *alive_cells_t);
+
+#endif
---
+++ Assignment2/GameOfLife/h_extension.h
@@ -0,0 +1,14 @@
+//
+//  v_extension.h
+//  GameOfLife
+//
+//  Created by JIANNAN GUO on 10.05.13.
+//  Copyright (c) 2013 Jiannan Guo. All rights reserved.
+//
+
+#ifndef GameOfLife_v_extension_h
+#define GameOfLife_v_extension_h
+
+int h_extend(struct cell_list *local_alive_l);
+
+#endif
