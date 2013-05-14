189fe8d6b3318f932af224a5c9967a357d05e4a1
Ramkumar <cbe.ram@gmail.com>
2013-05-14 15:09:02 +0200

    Modified main.c, added a couple of header files for command line argument parsing and initializing the MPI environment. Also added a input pattern file for testing.

---
+++ 2/GameOfLife/initialize.h
@@ -0,0 +1,128 @@
+#ifndef LIFE_H
+#define LIFE_H
+
+
+#include "defaults.h" // For Life's constants
+
+#include <stdlib.h>   // For malloc et al.
+#include <stdbool.h>  // For true/false
+#include <getopt.h>   // For argument processing
+#include <stdio.h>    // For file i/o
+
+
+void        init_grids (struct cell_list * world, char * infile);
+void        write_grid (struct cell_list * world, char *outfile);
+void        parse_args (struct args_t * args, int argc, char ** argv);
+void        usage ();
+
+/*
+		Initialize cells based on input file, otherwise all cells
+		are DEAD.
+*/
+void init_grids (struct cell_list * world, char * infile) {
+	FILE * fd;
+	int i,j,index=0;
+
+	world->len = (int)calloc(1, sizeof(world->len));
+
+	if (infile != NULL) {
+		if ((fd = fopen(infile, "r")) == NULL) {
+			perror("Failed to open file for input");
+			exit(EXIT_FAILURE);
+		}
+
+		if (fscanf(fd, "%d\n", &world->len) == EOF) {
+			printf("File must at least define grid dimensions!\nExiting.\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	world->ptr = (struct cell *) calloc((world->len), sizeof(struct cell));
+
+	if (infile != NULL) {
+		while (fscanf(fd, "%d %d\n", &i, &j) != EOF) {
+			world->ptr[index].h = i;
+			world->ptr[index].v = j;
+			index++;
+		}
+		
+		fclose(fd);
+	}
+}
+
+/*
+		Dumps the current state of the world to outfile.
+		Only outputs the coordinates of ALIVE cells.
+*/
+void write_grid (struct cell_list *world, char *outfile) {
+	FILE * fd;
+	int i;
+
+	if (outfile != NULL) {
+		if ((fd = fopen(outfile, "w")) == NULL) {
+			perror("Failed to open file for output");
+			exit(EXIT_FAILURE);
+		}
+
+		for (i = 0; i < world->len; i++) {
+			fprintf(fd, "%d %d\n", world->ptr[i].h, world->ptr[i].v);
+		}
+
+		fclose(fd);
+	}
+}
+
+/*
+	usage()
+		Describes Life's command line option
+*/
+void usage () {
+	printf("\nUsage: Life [options]\n");
+	printf("  -c|--columns <number>   Number of columns in grid. Default: %d\n", DEFAULT_COLS);
+	printf("  -r|--rows <number>      Number of rows in grid. Default: %d\n", DEFAULT_ROWS);
+	printf("  -g|--gens <number>      Number of generations to run. Default: %d\n", DEFAULT_GENS);
+	printf("  -i|--input <filename>   Input pattern file.\n");
+	printf("  -o|--output <filename>  Output file.\n");
+
+	exit(EXIT_FAILURE);
+}
+
+/*
+		Parse command line arguments
+*/
+void parse_args (struct args_t * args, int argc, char ** argv) {
+	int opt       = 0;
+	int opt_index = 0;
+	for (;;) {
+		opt = getopt_long(argc, argv, opts, long_opts, &opt_index);
+
+		if (opt == -1) break;
+
+		switch (opt) {
+			case 'c':
+				args->ncols = strtol(optarg, (char**) NULL, 10);
+				break;
+			case 'r':
+				args->nrows = strtol(optarg, (char**) NULL, 10);
+				break;
+			case 'g':
+				args->ngens = strtol(optarg, (char**) NULL, 10);
+				break;
+			case 'i':
+				args->infile = optarg;
+				break;
+			case 'o':
+				args->outfile = optarg;
+				break;
+			case 'h':
+			case '?':
+				usage();
+				break;
+
+			default:
+				break;
+		}
+	}
+}
+
+#endif
--- 2/GameOfLife/main.c
+++ 2/GameOfLife/main.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "localize.h"
 #include "cell_struct.h"
 //#include "h_extension.h"
@@ -18,12 +19,20 @@
 #include "msg_extract.h"
 #include "denormalize.h"
 #include "killer.h"
+#include "defaults.h"
+#include "initialize.h"
+#include <mpi.h>
+#include <getopt.h>
 
 int COUNT_FIELD[COUNT_HEIGHT][COUNT_LENGTH];
 
-int main()
-{
-    struct cell fullfield[10] = {
+int main(int argc, char ** argv) {
+
+        struct args_t args = {DEFAULT_ROWS, DEFAULT_COLS, DEFAULT_GENS, NULL, NULL};
+        struct cell_list wholefield_l;
+        int rank, size;
+
+/*    struct cell fullfield[10] = {
         {4,2},
         {4,3},
         {5,2},
@@ -39,101 +48,115 @@ int main()
     struct cell_list wholefield_l = {
         10,
         fullfield
-    };
+    }; */
     
-    printf("Process %d\n", SEQ_OF_PROCESS);
-    
-    cell_list *current = NULL;
-    cell_list *next = NULL;
-    
-    message *up, *down;
-    
-    /* --------------localize-------------- */
-    
-    current = divide_field(SEQ_OF_PROCESS, &wholefield_l);
+    //printf("Process %d\n", SEQ_OF_PROCESS);
 
-    normalize(SEQ_OF_PROCESS, current);
+        MPI_Init(&argc, &argv);
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+        parse_args(&args, argc, argv);
+
+        printf("The arguments are:\n%d, %d, %d, %s, %s\n", args.nrows, args.ncols, args.ngens, args.infile, args.outfile);
+
+        init_grids(&wholefield_l, args.infile);
+
+        printf("\nThe number of cells initially are: %d\n", wholefield_l.len);
+        for(i=0; i<wholefield_l.len; i++)
+                printf("%d %d\n", wholefield_l.ptr[i].h, wholefield_l.ptr[i].v);
     
-    printcell(current, "local job");
-    
-    /* --------------go through a cycle of evolution-------------- */
-    
-    next = evolve(current);
-    
-    printcell(next, "1st generation");
-    
-    printcell_vividly(next, "1st generation");
-    
-    // generate the message
-    
-    down = msg_extract("down", next);
-    
-    printf("down message: num = %d\n", down->num);
-    
-    up = msg_extract("up", next);
-    
-    printf("up message: num = %d\n", up->num);
-    
-    kill_msg(down);
-    
-    kill_msg(up);
-    
-    /* --------------------------end------------------------------ */
-    
-    current = next;
-    
-    /* --------------go through a cycle of evolution-------------- */
-    
-    next = evolve(current);
-    
-    printcell(next, "2nd generation");
-    
-    printcell_vividly(next, "2nd generation");
-    
-    // generate the message
-    
-    down = msg_extract("down", next);
-    
-    printf("down message: num = %d\n", down->num);
-    
-    up = msg_extract("up", next);
-    
-    printf("up message: num = %d\n", up->num);
-    
-    kill_msg(down);
-    
-    kill_msg(up);
-    
-    /* --------------------------end------------------------------ */
-    
-    current = next;
-    
-    /* ------------ for memory debugger ----------- */
-    /*
-    while (1) {
-        next = evolve(current);
-        kill_cell(current);
-        current = next;
-    }
-    */
-    
-    for (int i=0; i<1000000; i++) {
-        next = evolve(current);
-        current = next;
-    }
-    /* -------------------end-------------------- */
-    
-    /* --------------denormalize-------------- */
-    
-    cell_list *origin;
-    
-    origin = denormalize(SEQ_OF_PROCESS, next);
-    
-    printcell(origin, "coordinates in standard format");
-    
-    kill_cell(next);
-    
-    kill_cell(origin);
+//    cell_list *current = NULL;
+//    cell_list *next = NULL;
+//    
+//    message *up, *down;
+//    
+//    /* --------------localize-------------- */
+//    
+//    current = divide_field(SEQ_OF_PROCESS, &wholefield_l);
+//
+//    normalize(SEQ_OF_PROCESS, current);
+//    
+//    printcell(current, "local job");
+//    
+//    /* --------------go through a cycle of evolution-------------- */
+//    
+//    next = evolve(current);
+//    
+//    printcell(next, "1st generation");
+//    
+//    printcell_vividly(next, "1st generation");
+//    
+//    // generate the message
+//    
+//    down = msg_extract("down", next);
+//    
+//    printf("down message: num = %d\n", down->num);
+//    
+//    up = msg_extract("up", next);
+//    
+//    printf("up message: num = %d\n", up->num);
+//    
+//    kill_msg(down);
+//    
+//    kill_msg(up);
+//    
+//    /* --------------------------end------------------------------ */
+//    
+//    current = next;
+//    
+//    /* --------------go through a cycle of evolution-------------- */
+//    
+//    next = evolve(current);
+//    
+//    printcell(next, "2nd generation");
+//    
+//    printcell_vividly(next, "2nd generation");
+//    
+//    // generate the message
+//    
+//    down = msg_extract("down", next);
+//    
+//    printf("down message: num = %d\n", down->num);
+//    
+//    up = msg_extract("up", next);
+//    
+//    printf("up message: num = %d\n", up->num);
+//    
+//    kill_msg(down);
+//    
+//    kill_msg(up);
+//    
+//    /* --------------------------end------------------------------ */
+//    
+//    current = next;
+//    
+//    /* ------------ for memory debugger ----------- */
+//    /*
+//    while (1) {
+//        next = evolve(current);
+//        kill_cell(current);
+//        current = next;
+//    }
+//    */
+//    
+//    for (int i=0; i<1000000; i++) {
+//        next = evolve(current);
+//        current = next;
+//    }
+//    /* -------------------end-------------------- */
+//    
+//    /* --------------denormalize-------------- */
+//    
+//    cell_list *origin;
+//    
+//    origin = denormalize(SEQ_OF_PROCESS, next);
+//    
+//    printcell(origin, "coordinates in standard format");
+//    
+//    kill_cell(next);
+//    
+//    kill_cell(origin);
     
     return 0;
 }
